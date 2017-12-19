cf7cf7bbbbfc013be0a5204da1e90685b747143b
Amir Khoury <amirkhourykh@gmail.com>
2017-12-19 12:46:58 +0200

    Added the folder sand files: public/css/style.css public/js and added some content to the css and to the profile.hbs page. Reference #3.

--- src/controller/router.js
+++ src/controller/router.js
@@ -6,6 +6,11 @@ router.get('/', (req, res) => {
   res.render('home', {title:'homepage', users:['amir', 'ghassan']},);
 })
 
+router.get('/:name', (req, res) => {
+  const name = req.params.name;
+  res.render('profile', {user:name, title:`profile of ${name}`})
+})
+
 router.get('*', (req, res) => {
   res.send('page not found');
 })

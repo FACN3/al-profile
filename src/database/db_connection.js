6dcc0bd15319b79eb2e7b92943e44abf2cdd91fe
Ghassan <ghassan.maslamani@gmail.com>
2017-12-21 12:10:26 +0200

    solving some conflicts

--- src/database/db_connection.js
+++ src/database/db_connection.js
@@ -1,10 +1,10 @@
-env('./config.env');
-const env = require('env2');
-const { Pool } = require('pg');
+const env = require("env2");
+env("./config.env");
+const { Pool } = require("pg");
 let DB_URL = process.env.DB_URL;
 
 if (!DB_URL) {
-  throw new Error('environment variable DB_URL must be set');
+  throw new Error("environment variable DB_URL must be set");
 }
 
 const pool = new Pool({
